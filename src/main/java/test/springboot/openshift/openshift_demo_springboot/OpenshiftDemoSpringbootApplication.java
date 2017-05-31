74bfdd77ff23ff89c99c4069ae5173e854344d25
RJ <rj@rj.com>
2017-05-31 11:14:07 -0400

    Added Spring nature to GIT project

---
+++ src/main/java/test/springboot/openshift/openshift_demo_springboot/OpenshiftDemoSpringbootApplication.java
@@ -0,0 +1,12 @@
+package test.springboot.openshift.openshift_demo_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftDemoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftDemoSpringbootApplication.class, args);
+	}
+}
