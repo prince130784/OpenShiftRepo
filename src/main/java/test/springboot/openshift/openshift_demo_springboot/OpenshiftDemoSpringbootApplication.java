fb88e1368e3838bd7ac52d7b1a584a0fc643fb31
RJ <rj@rj.com>
2017-05-31 15:18:27 -0400

    Commiting the changes to working directory

--- src/main/java/test/springboot/openshift/openshift_demo_springboot/OpenshiftDemoSpringbootApplication.java
+++ src/main/java/test/springboot/openshift/openshift_demo_springboot/OpenshiftDemoSpringbootApplication.java
@@ -2,11 +2,19 @@ package test.springboot.openshift.openshift_demo_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class OpenshiftDemoSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftDemoSpringbootApplication.class, args);
+	}
+	
+	
+	
+	@GetMapping("/")
+	public String response(){
+		return "Hello Ravinderjeet!! Welcome to your first Cloup App";
 	}
 }
