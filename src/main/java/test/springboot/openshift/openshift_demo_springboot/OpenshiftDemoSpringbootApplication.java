b42b5349f567e22b569ae54795429ebbdef58e2e
RJ <rj@rj.com>
2017-06-05 11:08:00 -0400

    adding new files to working directory

--- src/main/java/test/springboot/openshift/openshift_demo_springboot/OpenshiftDemoSpringbootApplication.java
+++ src/main/java/test/springboot/openshift/openshift_demo_springboot/OpenshiftDemoSpringbootApplication.java
@@ -3,6 +3,7 @@ package test.springboot.openshift.openshift_demo_springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class OpenshiftDemoSpringbootApplication {
@@ -11,7 +12,10 @@ public class OpenshiftDemoSpringbootApplication {
 		SpringApplication.run(OpenshiftDemoSpringbootApplication.class, args);
 	}
 	
-	
+}
+
+@RestController
+class HomeController{
 	
 	@GetMapping("/")
 	public String response(){
